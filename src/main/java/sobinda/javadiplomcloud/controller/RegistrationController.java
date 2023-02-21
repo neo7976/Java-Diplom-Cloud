ca3e117dfb3d56f20f2589458988bde7222ad676
Dmitrii Sobin <neo7976@yandex.ru>
2023-02-21 17:15:32 +0300

    RegistrationController

---
+++ src/main/java/sobinda/javadiplomcloud/controller/RegistrationController.java
@@ -0,0 +1,43 @@
+package sobinda.javadiplomcloud.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import sobinda.javadiplomcloud.entity.User;
+import sobinda.javadiplomcloud.model.Login;
+import sobinda.javadiplomcloud.service.UserService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequiredArgsConstructor
+public class RegistrationController {
+    Logger logger;
+    private final UserService userService;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(@ModelAttribute("userForm") @Validated User userForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
+            model.addAttribute("passwordError", "Пароли не совпадают");
+            return "registration";
+        }
+        if (!userService.saveUser(userForm)) {
+            model.addAttribute("userLoginError", "Пользователь с таким именем уже существует");
+            return "registration";
+        }
+        return "redirect:/";
+    }
+}
+
