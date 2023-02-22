765b08ae9af510ae9f0e96d76ff636e5a7604793
Dmitrii Sobin <neo7976@yandex.ru>
2023-02-22 13:04:41 +0300

    Добавлены роли

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,18 @@
 create schema if not exists diploma_cloud;
 
 
-insert into users(login, password) values ('neo8745@yandex.ru', '$2a$10$ciFUmq726O5V0Tnu37uEy.I7nhPXWO/WFN8m7HjNOphZFcOQcrl8a');
-insert into users(login, password) values ('panya12@yandex.ru', '$2a$10$L4cA.wDXaxBESV/FUGchT.WyEFX6qgMrdGGjDl7kt9QMFVWobi5Ne');
-insert into users(login, password) values ('meloman7575@yandex.ru', '$2a$10$fHYr2Lk2.KwBM4EFgQt97eb0rfOSVFl438DZbYIuE/hXlFL9mH6XG');
+insert into users(login, password)
+values ('neo8745@yandex.ru', '$2a$10$ciFUmq726O5V0Tnu37uEy.I7nhPXWO/WFN8m7HjNOphZFcOQcrl8a');
+insert into users(login, password)
+values ('panya12@yandex.ru', '$2a$10$L4cA.wDXaxBESV/FUGchT.WyEFX6qgMrdGGjDl7kt9QMFVWobi5Ne');
+insert into users(login, password)
+values ('meloman7575@yandex.ru', '$2a$10$fHYr2Lk2.KwBM4EFgQt97eb0rfOSVFl438DZbYIuE/hXlFL9mH6XG');
+
+insert into user_entity_roles(user_entity_id, roles)
+values (1, 'ROLE_ADMIN');
+insert into user_entity_roles(user_entity_id, roles)
+values (1, 'ROLE_WRITE');
+insert into user_entity_roles(user_entity_id, roles)
+values (2, 'ROLE_WRITE');
+insert into user_entity_roles(user_entity_id, roles)
+values (3, 'ROLE_READE');
