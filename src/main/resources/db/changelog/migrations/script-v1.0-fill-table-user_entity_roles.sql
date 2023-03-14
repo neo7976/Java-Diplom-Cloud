5773404793fe0782abf920d1ec5a6b8e54e0bde9
Dmitrii Sobin <neo7976@yandex.ru>
2023-03-14 15:41:01 +0300

    'liquibase'

---
+++ src/main/resources/db/changelog/migrations/script-v1.0-fill-table-user_entity_roles.sql
@@ -0,0 +1,9 @@
+insert into user_entity_roles(user_entity_id, roles)
+values (1, 'ROLE_ADMIN');
+insert into user_entity_roles(user_entity_id, roles)
+values (1, 'ROLE_WRITE');
+insert into user_entity_roles(user_entity_id, roles)
+values (2, 'ROLE_WRITE');
+insert into user_entity_roles(user_entity_id, roles)
+values (3, 'ROLE_READ');
+
--- src/main/resources/db/changelog/migrations/script-v1.0-fill-table-users.sql
+++ src/main/resources/db/changelog/migrations/script-v1.0-fill-table-users.sql
@@ -1,6 +1,3 @@
-create schema if not exists diploma_cloud;
-
-
 insert into users(login, password)
 values ('neo8745@yandex.ru', '$2a$10$ciFUmq726O5V0Tnu37uEy.I7nhPXWO/WFN8m7HjNOphZFcOQcrl8a');
 insert into users(login, password)
@@ -8,12 +5,7 @@ values ('panya12@yandex.ru', '$2a$10$L4cA.wDXaxBESV/FUGchT.WyEFX6qgMrdGGjDl7kt9Q
 insert into users(login, password)
 values ('meloman7575@yandex.ru', '$2a$10$fHYr2Lk2.KwBM4EFgQt97eb0rfOSVFl438DZbYIuE/hXlFL9mH6XG');
 
-insert into user_entity_roles(user_entity_id, roles)
-values (1, 'ROLE_ADMIN');
-insert into user_entity_roles(user_entity_id, roles)
-values (1, 'ROLE_WRITE');
-insert into user_entity_roles(user_entity_id, roles)
-values (2, 'ROLE_WRITE');
-insert into user_entity_roles(user_entity_id, roles)
-values (3, 'ROLE_READ');
 
+# neo8745@yandex.ru        Исходный пароль:  12345678
+# panya12@yandex.ru        Исходный пароль: 1111222
+# meloman7575@yandex.ru    Исходный пароль: 12344321
